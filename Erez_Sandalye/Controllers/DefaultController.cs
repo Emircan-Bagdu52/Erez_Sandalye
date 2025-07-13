68dc322f6680ff024e390a5367dc99f0763d2af2
Emircan Bağdu <emicanbagdu52@gmail.com>
2025-07-14 00:08:43 +0300

    UI Düzenlemeleri

---
+++ Erez_Sandalye/Controllers/DefaultController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Erez_Sandalye.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult HeaderPartial()
+		{
+			return PartialView();
+		}
+		public PartialViewResult ScriptPartial()
+		{
+			return PartialView();
+		}
+		public PartialViewResult FeatureSection()
+		{
+			return PartialView();
+		}
+		public PartialViewResult SliderPartial()
+		{
+			return PartialView();
+		}
+	}
+}
